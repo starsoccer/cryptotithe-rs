49ab9dece5a5a136756726c8c64ff0b1ea014688
starsoccer <1938666+starsoccer@users.noreply.github.com>
2021-05-08 16:14:48 -0400

    Fix bug in HTFO

--- src/holding_selection/get_currency_holding/highest_tax_first_out.rs
+++ src/holding_selection/get_currency_holding/highest_tax_first_out.rs
@@ -1,24 +1,53 @@
-use crate::trade::Trade;
 use crate::holding::CurrencyHolding;
+use crate::trade::Trade;
 use crate::YEAR_IN_MILLISECONDS;
 
-pub fn highest_tax_first_out (trade: Trade, currency_holdings: &Vec<CurrencyHolding>) -> usize {
-    currency_holdings.iter().enumerate().fold(
-        (0, None), |(highest_index, unsafe_highest_tax_holding): (usize, Option<&CurrencyHolding>), (index, current_currency_holding)| {
-            if let Some(highest_tax_holding) = unsafe_highest_tax_holding {
-                if trade.date.wrapping_sub(current_currency_holding.date) >= YEAR_IN_MILLISECONDS {
-                    if current_currency_holding.rate_in_fiat < highest_tax_holding.rate_in_fiat {
-                        return (index, Some(current_currency_holding));
+pub fn highest_tax_first_out(trade: Trade, currency_holdings: &Vec<CurrencyHolding>) -> usize {
+    currency_holdings
+        .iter()
+        .enumerate()
+        .fold(
+            (0, None),
+            |(highest_index, unsafe_highest_tax_holding): (usize, Option<&CurrencyHolding>),
+             (index, current_currency_holding)| {
+                if let Some(highest_tax_holding) = unsafe_highest_tax_holding {
+                    if trade.date.wrapping_sub(current_currency_holding.date)
+                        >= YEAR_IN_MILLISECONDS
+                    {
+                        // current holding triggers long term gains
+                        if trade.date.wrapping_sub(highest_tax_holding.date) >= YEAR_IN_MILLISECONDS &&
+                            current_currency_holding.rate_in_fiat < highest_tax_holding.rate_in_fiat
+                        {
+                            return (index, Some(current_currency_holding)); 
+                        }
+                        return (highest_index, Some(highest_tax_holding)); // current holding isnt highest
+                    } else {
+                        // current holding is short term
+                        if trade.date.wrapping_sub(highest_tax_holding.date) >= YEAR_IN_MILLISECONDS {
+                            // current highest is long term but short term tax is always higher
+                            return (index, Some(current_currency_holding));
+                        } else {
+                            if current_currency_holding.rate_in_fiat < highest_tax_holding.rate_in_fiat
+                            {
+                                return (index, Some(current_currency_holding)); // current holding rate in fiat is lower meaning higher cost
+                            }
+                            return (highest_index, Some(highest_tax_holding));
+                        }
                     }
-                    return (highest_index, Some(highest_tax_holding));
-                } else if trade.date.wrapping_sub(highest_tax_holding.date) >= YEAR_IN_MILLISECONDS {
-                    return (index, Some(current_currency_holding));
                 } else {
-                    return (highest_index, Some(highest_tax_holding));
+                    return (index, Some(current_currency_holding));
                 }
-            } else {
-                return (index, Some(current_currency_holding));
-            }
-        }
-    ).0
+            },
+        )
+        .0
 }
+
+/*
+                if (currentCurrencyHolding.rateInFiat < highestTax.rateInFiat) {
+                    highestTaxHoldingIndex = currentIndex;
+                    return currentCurrencyHolding;
+                }
+                return highestTax;
+                
+
+*/
